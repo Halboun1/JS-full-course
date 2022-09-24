411fd7034e81547a779974b2a37114d4b3993582
Boun13a <halbounicd@gmail.com>
2022-09-24 16:42:41 -0400

    finished the first chapter

---
+++ chapter 1/jsValuesAndVariables.js
@@ -0,0 +1,12 @@
+/* ========== NUMBERS ========== */
+let x = 2.5;                // numbers with decimals
+let y = 3;                  // number without decimal
+
+let newX = 123e5;           // e5 represents how many 0's (12300000)
+let newY = 123e-5;          // e-5 represents the decimals number (0.00123)
+
+
+/* ========== STRINGS ========== */
+let name = "John Doe";      // name variable represents sequence of characters
+let carName = "Volvo XC60"  // double quotes are important to wrap around the variable value.
+let carName1 = 'Volvo XC60' // can also wrap it in single quotes.
