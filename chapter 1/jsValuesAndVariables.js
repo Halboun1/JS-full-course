33cf8cb89f979c470c3b48e8a7e499ead8282dd1
Boun13a <halbounicd@gmail.com>
2022-09-26 16:10:52 -0400

    working on chapter 3

--- chapter 1/jsValuesAndVariables.js
+++ chapter 1/jsValuesAndVariables.js
@@ -1,12 +1,18 @@
 /* ========== NUMBERS ========== */
-let x = 2.5;                // numbers with decimals
-let y = 3;                  // number without decimal
+let x = 2.5;                    // numbers with decimals
+let y = 3;                      // number without decimal
 
-let newX = 123e5;           // e5 represents how many 0's (12300000)
-let newY = 123e-5;          // e-5 represents the decimals number (0.00123)
+let newX = 123e5;               // e5 represents how many 0's (12300000)
+let newY = 123e-5;              // e-5 represents the decimals number (0.00123)
 
 
 /* ========== STRINGS ========== */
-let name = "John Doe";      // name variable represents sequence of characters
-let carName = "Volvo XC60"  // double quotes are important to wrap around the variable value.
-let carName1 = 'Volvo XC60' // can also wrap it in single quotes.
+let name = "John Doe";          // name variable represents sequence of characters
+let carName = "Volvo XC60"      // double quotes are important to wrap around the variable value.
+let carName1 = 'Volvo XC60'     // can also wrap it in single quotes.
+
+
+/* ========== CONST ========== */
+const hens = 4;                 // const works just like let, except you cannot change the value.
+                                // use const in code when you will not change the value through out the code.
+                                // example week days = 7, months = 12 or pi = 3.14159
